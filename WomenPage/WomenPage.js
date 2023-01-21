993bcdc356302950c2eed7ac8ac76c7a297d5ce7
sumit9235 <sy7985494155@gmail.com>
2023-01-21 20:05:19 +0530

    womens page done

---
+++ WomenPage/WomenPage.js
@@ -0,0 +1,10 @@
+function opennav(){
+    document.getElementById("myslide").style.width="250px"
+}
+
+function closenav(){
+    document.getElementById("myslide").style.width="0"
+}
+function Cart(){
+    document.location="/Bagpage/Bagpage.html"
+}
